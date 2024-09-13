2e814a71af1b495906698fa5429550ce26729216
giorgi19 <nadiradze19@gmail.com>
2024-09-13 14:02:17 +0400

    init project

---
+++ lib/app/view/app.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:pixabay_list/app/view/app_view.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const AppView();
+  }
+}
