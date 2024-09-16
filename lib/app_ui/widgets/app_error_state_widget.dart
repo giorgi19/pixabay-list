a72b0e1eaa9d0878b155c6635cc379ddf9ad230a
giorgi19 <nadiradze19@gmail.com>
2024-09-16 18:51:08 +0400

    feat: refactor error state widget

--- lib/app_ui/widgets/app_error_state_widget.dart
+++ lib/app_ui/widgets/app_error_state_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:pixabay_list/app_ui/typography/app_text_styles.dart';
 
 class AppErrorStateWidget extends StatelessWidget {
   const AppErrorStateWidget({
@@ -12,7 +13,12 @@ class AppErrorStateWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Text(error ?? 'error'),
+        child: Text(
+          // Needs to be replaced with user-friendly error messages.
+          // error ??
+          'Error',
+          style: UITextStyle.headline3,
+        ),
       ),
     );
   }
